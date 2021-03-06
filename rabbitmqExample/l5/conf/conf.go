062f7200348ccd56524bc03d63b732490d2467e2
brady <brady.wang@qq.com>
2021-03-07 01:19:36 +0800

    go

--- rabbitmqExample/l5/conf/conf.go
+++ rabbitmqExample/l5/conf/conf.go
@@ -1,7 +1,7 @@
 package config
 
 const (
-	RMQADDR     = "amqp://admin:123456@127.0.0.1:5672/"
-	EXCHANGENAME = "syslog_topic"
-	ROUTEKEY = "test_route_key"
+	RMQADDR     = "amqp://admin:123456@192.168.1.99:5672/"
+	EXCHANGENAME = "exchange_user"
+	ROUTEKEY = "route_user"
 )
